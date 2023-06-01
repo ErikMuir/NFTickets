933fe0d9363990c0bffb5b7b2faabc2f66d37e10
Erik Muir <erikdmuir@gmail.com>
2023-06-01 08:04:35 -0500

    import libraries

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -1,219 +1,224 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-struct PricingTemplate {
-  uint ticketPrice;
-  uint venueFlatFee;
-  uint entertainerFlatFee;
-  uint8 venuePercentage;
-  uint8 entertainerPercentage;
-}
+// struct PricingTemplate {
+//   uint ticketPrice;
+//   uint venueFlatFee;
+//   uint entertainerFlatFee;
+//   uint8 venuePercentage;
+//   uint8 entertainerPercentage;
+// }
 
-struct PricingTemplateMap {
-  string[] keys;
-  mapping(string key => PricingTemplate) values;
-  mapping(string key => uint) indexOf;
-  mapping(string key => bool) inserted;
-}
+// struct PricingTemplateMap {
+//   string[] keys;
+//   mapping(string key => PricingTemplate) values;
+//   mapping(string key => uint) indexOf;
+//   mapping(string key => bool) inserted;
+// }
 
-library PricingTemplateIterableMapping {
-  function get(PricingTemplateMap storage self, string memory key) public view returns (PricingTemplate storage) {
-    return self.values[key];
-  }
+// library PricingTemplateIterableMapping {
+//   function get(PricingTemplateMap storage self, string memory key) public view returns (PricingTemplate storage) {
+//     return self.values[key];
+//   }
 
-  function getKeyAtIndex(PricingTemplateMap storage self, uint index) public view returns (string storage) {
-    return self.keys[index];
-  }
+//   function getKeyAtIndex(PricingTemplateMap storage self, uint index) public view returns (string storage) {
+//     return self.keys[index];
+//   }
 
-  function size(PricingTemplateMap storage self) public view returns (uint) {
-    return self.keys.length;
-  }
+//   function size(PricingTemplateMap storage self) public view returns (uint) {
+//     return self.keys.length;
+//   }
 
-  function set(PricingTemplateMap storage self, string memory key, PricingTemplate memory val) public {
-    if (self.inserted[key]) {
-      self.values[key] = val;
-    } else {
-      self.inserted[key] = true;
-      self.values[key] = val;
-      self.indexOf[key] = self.keys.length;
-      self.keys.push(key);
-    }
-  }
+//   function set(PricingTemplateMap storage self, string memory key, PricingTemplate memory val) public {
+//     if (self.inserted[key]) {
+//       self.values[key] = val;
+//     } else {
+//       self.inserted[key] = true;
+//       self.values[key] = val;
+//       self.indexOf[key] = self.keys.length;
+//       self.keys.push(key);
+//     }
+//   }
 
-  function remove(PricingTemplateMap storage self, string memory key) public {
-    if (!self.inserted[key]) {
-      return;
-    }
+//   function remove(PricingTemplateMap storage self, string memory key) public {
+//     if (!self.inserted[key]) {
+//       return;
+//     }
 
-    delete self.inserted[key];
-    delete self.values[key];
+//     delete self.inserted[key];
+//     delete self.values[key];
 
-    uint index = self.indexOf[key];
-    string memory lastKey = self.keys[self.keys.length - 1];
+//     uint index = self.indexOf[key];
+//     string memory lastKey = self.keys[self.keys.length - 1];
 
-    self.indexOf[lastKey] = index;
-    delete self.indexOf[key];
+//     self.indexOf[lastKey] = index;
+//     delete self.indexOf[key];
 
-    self.keys[index] = lastKey;
-    self.keys.pop();
-  }
-}
+//     self.keys[index] = lastKey;
+//     self.keys.pop();
+//   }
+// }
 
-struct OpenSection {
-  string pricingTemplateKey;
-  uint maxCapacity;
-  uint remainingCapacity;
-}
+// struct OpenSection {
+//   string pricingTemplateKey;
+//   uint maxCapacity;
+//   uint remainingCapacity;
+// }
 
-struct OpenSectionMap {
-  string[] keys;
-  mapping(string key => OpenSection) values;
-  mapping(string key => uint) indexOf;
-  mapping(string key => bool) inserted;
-}
+// struct OpenSectionMap {
+//   string[] keys;
+//   mapping(string key => OpenSection) values;
+//   mapping(string key => uint) indexOf;
+//   mapping(string key => bool) inserted;
+// }
 
-library OpenSectionIterableMapping {
-  function get(OpenSectionMap storage self, string memory key) public view returns (OpenSection storage) {
-    return self.values[key];
-  }
+// library OpenSectionIterableMapping {
+//   function get(OpenSectionMap storage self, string memory key) public view returns (OpenSection storage) {
+//     return self.values[key];
+//   }
 
-  function getKeyAtIndex(OpenSectionMap storage self, uint index) public view returns (string storage) {
-    return self.keys[index];
-  }
+//   function getKeyAtIndex(OpenSectionMap storage self, uint index) public view returns (string storage) {
+//     return self.keys[index];
+//   }
 
-  function size(OpenSectionMap storage self) public view returns (uint) {
-    return self.keys.length;
-  }
+//   function size(OpenSectionMap storage self) public view returns (uint) {
+//     return self.keys.length;
+//   }
 
-  function set(OpenSectionMap storage self, string memory key, OpenSection memory val) public {
-    if (self.inserted[key]) {
-      self.values[key] = val;
-    } else {
-      self.inserted[key] = true;
-      self.values[key] = val;
-      self.indexOf[key] = self.keys.length;
-      self.keys.push(key);
-    }
-  }
+//   function set(OpenSectionMap storage self, string memory key, OpenSection memory val) public {
+//     if (self.inserted[key]) {
+//       self.values[key] = val;
+//     } else {
+//       self.inserted[key] = true;
+//       self.values[key] = val;
+//       self.indexOf[key] = self.keys.length;
+//       self.keys.push(key);
+//     }
+//   }
 
-  function remove(OpenSectionMap storage self, string memory key) public {
-    if (!self.inserted[key]) {
-      return;
-    }
+//   function remove(OpenSectionMap storage self, string memory key) public {
+//     if (!self.inserted[key]) {
+//       return;
+//     }
 
-    delete self.inserted[key];
-    delete self.values[key];
+//     delete self.inserted[key];
+//     delete self.values[key];
 
-    uint index = self.indexOf[key];
-    string memory lastKey = self.keys[self.keys.length - 1];
+//     uint index = self.indexOf[key];
+//     string memory lastKey = self.keys[self.keys.length - 1];
 
-    self.indexOf[lastKey] = index;
-    delete self.indexOf[key];
+//     self.indexOf[lastKey] = index;
+//     delete self.indexOf[key];
 
-    self.keys[index] = lastKey;
-    self.keys.pop();
-  }
-}
+//     self.keys[index] = lastKey;
+//     self.keys.pop();
+//   }
+// }
 
-struct ReservedSeatPricingMap {
-  string[] keys;
-  mapping(string key => string) values;
-  mapping(string key => uint) indexOf;
-  mapping(string key => bool) inserted;
-}
+// struct ReservedSeatPricingMap {
+//   string[] keys;
+//   mapping(string key => string) values;
+//   mapping(string key => uint) indexOf;
+//   mapping(string key => bool) inserted;
+// }
 
-library ReservedSeatPricingIterableMapping {
-  function get(ReservedSeatPricingMap storage self, string memory key) public view returns (string storage) {
-    return self.values[key];
-  }
+// library ReservedSeatPricingIterableMapping {
+//   function get(ReservedSeatPricingMap storage self, string memory key) public view returns (string storage) {
+//     return self.values[key];
+//   }
 
-  function getKeyAtIndex(ReservedSeatPricingMap storage self, uint index) public view returns (string storage) {
-    return self.keys[index];
-  }
+//   function getKeyAtIndex(ReservedSeatPricingMap storage self, uint index) public view returns (string storage) {
+//     return self.keys[index];
+//   }
 
-  function size(ReservedSeatPricingMap storage self) public view returns (uint) {
-    return self.keys.length;
-  }
+//   function size(ReservedSeatPricingMap storage self) public view returns (uint) {
+//     return self.keys.length;
+//   }
 
-  function set(ReservedSeatPricingMap storage self, string memory key, string memory val) public {
-    if (self.inserted[key]) {
-      self.values[key] = val;
-    } else {
-      self.inserted[key] = true;
-      self.values[key] = val;
-      self.indexOf[key] = self.keys.length;
-      self.keys.push(key);
-    }
-  }
+//   function set(ReservedSeatPricingMap storage self, string memory key, string memory val) public {
+//     if (self.inserted[key]) {
+//       self.values[key] = val;
+//     } else {
+//       self.inserted[key] = true;
+//       self.values[key] = val;
+//       self.indexOf[key] = self.keys.length;
+//       self.keys.push(key);
+//     }
+//   }
 
-  function remove(ReservedSeatPricingMap storage self, string memory key) public {
-    if (!self.inserted[key]) {
-      return;
-    }
+//   function remove(ReservedSeatPricingMap storage self, string memory key) public {
+//     if (!self.inserted[key]) {
+//       return;
+//     }
 
-    delete self.inserted[key];
-    delete self.values[key];
+//     delete self.inserted[key];
+//     delete self.values[key];
 
-    uint index = self.indexOf[key];
-    string memory lastKey = self.keys[self.keys.length - 1];
+//     uint index = self.indexOf[key];
+//     string memory lastKey = self.keys[self.keys.length - 1];
 
-    self.indexOf[lastKey] = index;
-    delete self.indexOf[key];
+//     self.indexOf[lastKey] = index;
+//     delete self.indexOf[key];
 
-    self.keys[index] = lastKey;
-    self.keys.pop();
-  }
-}
+//     self.keys[index] = lastKey;
+//     self.keys.pop();
+//   }
+// }
 
-struct ReservedSeatsMap {
-  string[] keys;
-  mapping(string key => uint) values;
-  mapping(string key => uint) indexOf;
-  mapping(string key => bool) inserted;
-}
+// struct ReservedSeatsMap {
+//   string[] keys;
+//   mapping(string key => uint) values;
+//   mapping(string key => uint) indexOf;
+//   mapping(string key => bool) inserted;
+// }
 
-library ReservedSeatsIterableMapping {
-  function get(ReservedSeatsMap storage self, string memory key) public view returns (uint) {
-    return self.values[key];
-  }
+// library ReservedSeatsIterableMapping {
+//   function get(ReservedSeatsMap storage self, string memory key) public view returns (uint) {
+//     return self.values[key];
+//   }
 
-  function getKeyAtIndex(ReservedSeatsMap storage self, uint index) public view returns (string storage) {
-    return self.keys[index];
-  }
+//   function getKeyAtIndex(ReservedSeatsMap storage self, uint index) public view returns (string storage) {
+//     return self.keys[index];
+//   }
 
-  function size(ReservedSeatsMap storage self) public view returns (uint) {
-    return self.keys.length;
-  }
+//   function size(ReservedSeatsMap storage self) public view returns (uint) {
+//     return self.keys.length;
+//   }
 
-  function set(ReservedSeatsMap storage self, string memory key, uint val) public {
-    if (self.inserted[key]) {
-      self.values[key] = val;
-    } else {
-      self.inserted[key] = true;
-      self.values[key] = val;
-      self.indexOf[key] = self.keys.length;
-      self.keys.push(key);
-    }
-  }
+//   function set(ReservedSeatsMap storage self, string memory key, uint val) public {
+//     if (self.inserted[key]) {
+//       self.values[key] = val;
+//     } else {
+//       self.inserted[key] = true;
+//       self.values[key] = val;
+//       self.indexOf[key] = self.keys.length;
+//       self.keys.push(key);
+//     }
+//   }
 
-  function remove(ReservedSeatsMap storage self, string memory key) public {
-    if (!self.inserted[key]) {
-      return;
-    }
+//   function remove(ReservedSeatsMap storage self, string memory key) public {
+//     if (!self.inserted[key]) {
+//       return;
+//     }
 
-    delete self.inserted[key];
-    delete self.values[key];
+//     delete self.inserted[key];
+//     delete self.values[key];
 
-    uint index = self.indexOf[key];
-    string memory lastKey = self.keys[self.keys.length - 1];
+//     uint index = self.indexOf[key];
+//     string memory lastKey = self.keys[self.keys.length - 1];
 
-    self.indexOf[lastKey] = index;
-    delete self.indexOf[key];
+//     self.indexOf[lastKey] = index;
+//     delete self.indexOf[key];
 
-    self.keys[index] = lastKey;
-    self.keys.pop();
-  }
-}
+//     self.keys[index] = lastKey;
+//     self.keys.pop();
+//   }
+// }
+
+import { PricingTemplate, PricingTemplateMap, PricingTemplateIterableMapping } from "./PricingTemplate.sol";
+import { OpenSection, OpenSectionMap, OpenSectionIterableMapping } from "./OpenSection.sol";
+import { ReservedSeatPricingMap, ReservedSeatPricingIterableMapping } from "./ReservedSeatPricing.sol";
+import { ReservedSeatsMap, ReservedSeatsIterableMapping } from "./ReservedSeats.sol";
 
 contract Event {
   // usings
@@ -238,6 +243,7 @@ contract Event {
   OpenSectionMap openSections;
   ReservedSeatPricingMap reservedSeatPricing;
   ReservedSeatsMap reservedSeats;
+  string defaultPricingTemplateKey;
   string tokenId;
 
   // constructor
@@ -281,6 +287,12 @@ contract Event {
     _;
   }
 
+  modifier readyToSign() {
+    bool defaultPricingTemplateKeySet = keccak256(abi.encode(defaultPricingTemplateKey)) != keccak256(abi.encode(""));
+    require(eventDateTime > 0 && defaultPricingTemplateKeySet, "Contract not ready to sign");
+    _;
+  }
+
   modifier resetSignatures() {
     _;
     venueSigned = false;
@@ -315,11 +327,11 @@ contract Event {
   }
 
   // admin functions
-  function venueSign() external onlyVenue notFinalized {
+  function venueSign() external onlyVenue notFinalized readyToSign {
     venueSigned = true;
   }
 
-  function entertainerSign() external onlyEntertainer notFinalized {
+  function entertainerSign() external onlyEntertainer notFinalized readyToSign {
     entertainerSigned = true;
   }
 
@@ -345,6 +357,11 @@ contract Event {
 
   function setReservedSeatPricing(string calldata _key, string calldata _pricingTemplateKey) external onlyAdmin notFinalized resetSignatures {
     reservedSeatPricing.set(_key, _pricingTemplateKey);
+  }
+
+  function setDefaultPricingTemplateKey(string calldata _pricingTemplateKey) external onlyAdmin notFinalized resetSignatures {
+    require(pricingTemplates.get(_pricingTemplateKey).ticketPrice > 0, "Unknown pricing template");
+    defaultPricingTemplateKey = _pricingTemplateKey;
   }
 
   // internal functions

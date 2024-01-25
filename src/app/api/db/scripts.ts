65a625e5269c4787c2c1a60ae94da718f47a759d
Erik Muir <erik.muir@omnifederal.com>
2024-01-25 10:00:22 -0600

    fix demo data

--- src/app/api/db/scripts.ts
+++ src/app/api/db/scripts.ts
@@ -1,13 +1,11 @@
+import { EntertainerType } from "@/clients/db/types";
 import { QueryResult, sql } from "@vercel/postgres";
 
 export const createWalletsTable = (): Promise<QueryResult> => {
   return sql`
     CREATE TABLE IF NOT EXISTS wallets (
-      id         uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-      address    text NOT NULL UNIQUE,
-      role       text NULL,
-      created_at text NOT NULL DEFAULT timezone('utc', now()),
-      updated_at text NOT NULL DEFAULT timezone('utc', now())
+      account text PRIMARY KEY NOT NULL,
+      role    text NULL
     );
   `;
 };
@@ -15,16 +13,10 @@ export const createWalletsTable = (): Promise<QueryResult> => {
 export const createVenuesTable = async (): Promise<QueryResult> => {
   return sql`
     CREATE TABLE IF NOT EXISTS venues (
-      id           uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-      wallet_id    uuid NOT NULL UNIQUE REFERENCES wallets ON DELETE CASCADE,
-      name         text NOT NULL,
-      description  text NULL,
-      address      text NULL,
-      city         text NULL,
-      state        text NULL,
-      zip          text NULL,
-      created_at   text NOT NULL DEFAULT timezone('utc', now()),
-      updated_at   text NOT NULL DEFAULT timezone('utc', now())
+      account     text PRIMARY KEY NOT NULL REFERENCES wallets,
+      name        text NOT NULL,
+      description text NULL,
+      location    text NULL
     );
   `;
 };
@@ -32,14 +24,36 @@ export const createVenuesTable = async (): Promise<QueryResult> => {
 export const createEntertainersTable = async (): Promise<QueryResult> => {
   return sql`
     CREATE TABLE IF NOT EXISTS entertainers (
-      id          uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-      wallet_id   uuid NOT NULL UNIQUE REFERENCES wallets ON DELETE CASCADE,
+      account     text PRIMARY KEY NOT NULL REFERENCES wallets,
       type        text NOT NULL,
       name        text NOT NULL,
       description text NULL,
-      iteration   text NULL,
-      created_at  text NOT NULL DEFAULT timezone('utc', now()),
-      updated_at  text NOT NULL DEFAULT timezone('utc', now())
+      iteration   text NULL
+    );
+  `;
+};
+
+export const createEventsTable = async (): Promise<QueryResult> => {
+  return sql`
+    CERATE TABLE IF NOT EXISTS events (
+      address     text PRIMARY KEY NOT NULL,
+      venue       text NOT NULL REFERENCES venues,
+      entertainer text NOT NULL REFERENCES entertainers
+    );
+  `;
+};
+
+export const createTicketsTable = async (): Promise<QueryResult> => {
+  return sql`
+    CREATE TABLE IF NOT EXISTS tickets (
+      token       text NOT NULL,
+      serial      integer NOT NULL,
+      venue       text NOT NULL REFERENCES venues,
+      entertainer text NOT NULL REFERENCES entertainers,
+      event       text NOT NULL REFERENCES events,
+      scanned_at  text NULL,
+      scanned_by  text NULL,
+      PRIMARY KEY(token, serial)
     );
   `;
 };
@@ -48,38 +62,75 @@ export const deleteAllWallets = async (): Promise<QueryResult> => {
   return sql`DELETE FROM wallets;`;
 };
 
+export const dropTicketsTable = async (): Promise<QueryResult> => {
+  return sql`DROP TABLE IF EXISTS tickets;`;
+};
+
+export const dropEventsTable = async (): Promise<QueryResult> => {
+  return sql`DROP TABLE IF EXISTS events;`;
+};
+
 export const dropVenuesTable = async (): Promise<QueryResult> => {
-  return sql`DROP TABLE IF EXISTS venues CASCADE;`;
+  return sql`DROP TABLE IF EXISTS venues;`;
 };
 
 export const dropEntertainersTable = async (): Promise<QueryResult> => {
-  return sql`DROP TABLE IF EXISTS entertainers CASCADE;`;
+  return sql`DROP TABLE IF EXISTS entertainers;`;
 };
 
 export const dropWalletsTable = async (): Promise<QueryResult> => {
-  return sql`DROP TABLE IF EXISTS wallets CASCADE;`;
+  return sql`DROP TABLE IF EXISTS wallets;`;
 };
 
-export const insertWallet = async (address: string): Promise<QueryResult> => {
-  return sql`INSERT INTO wallets ( address ) VALUES ( ${address} );`;
+export const insertWallet = async (account: string): Promise<QueryResult> => {
+  return sql`INSERT INTO wallets ( account ) VALUES ( ${account} );`;
 };
 
-export const insertVenue = async (address: string): Promise<QueryResult> => {
-  return sql`INSERT INTO venues ( walletId ) SELECT id FROM wallets WHERE address = ${address};`;
+export const insertVenue = async (
+  account: string,
+  name: string
+): Promise<QueryResult> => {
+  return sql`
+    INSERT INTO venues ( account, name )
+    SELECT account, ${name} FROM wallets WHERE account = ${account};
+  `;
 };
 
-export const insertEntertainer = async (address: string): Promise<QueryResult> => {
-  return sql`INSERT INTO entertainers ( walletId ) SELECT id FROM wallets WHERE address = ${address};`;
+export const insertEntertainer = async (
+  account: string,
+  name: string,
+  type: EntertainerType
+): Promise<QueryResult> => {
+  return sql`
+    INSERT INTO entertainers ( account, name, type )
+    SELECT account, ${name}, ${type} FROM wallets WHERE account = ${account};
+  `;
 };
 
-export const removeWallet = async (address: string): Promise<QueryResult> => {
-  return sql`DELETE FROM wallets WHERE address = ${address};`;
+export const insertTicket = async (
+  token: string,
+  serial: number
+): Promise<QueryResult> => {
+  return sql`INSERT INTO tickets ( token, serial ) VALUES ( ${token}, ${serial} );`;
 };
 
-export const removeVenue = async (address: string): Promise<QueryResult> => {
-  return sql`DELETE FROM venues WHERE walletId = (SELECT id FROM wallets WHERE address = ${address});`;
+export const removeWallet = async (account: string): Promise<QueryResult> => {
+  return sql`DELETE FROM wallets WHERE account = ${account};`;
 };
 
-export const removeEntertainer = async (address: string): Promise<QueryResult> => {
-  return sql`DELETE FROM entertainers WHERE walletId = (SELECT id FROM wallets WHERE address = ${address});`;
+export const removeVenue = async (account: string): Promise<QueryResult> => {
+  return sql`DELETE FROM venues WHERE account = ${account};`;
+};
+
+export const removeEntertainer = async (
+  account: string
+): Promise<QueryResult> => {
+  return sql`DELETE FROM entertainers WHERE account = ${account};`;
+};
+
+export const removeTicket = async (
+  token: string,
+  serial: number
+): Promise<QueryResult> => {
+  return sql`DELETE FROM tickets WHERE token = ${token} AND serial = ${serial}`;
 };
--- src/demo-data/entertainers.ts
+++ src/demo-data/entertainers.ts
@@ -1,22 +1,38 @@
-import { Entertainer, EntertainerType } from "@/types";
+import { Entertainer } from "@/clients/db/models";
+import { EntertainerType } from "@/clients/db/types";
 
 export const entertainers: Record<string, Entertainer> = {
   "0.0.111": {
+    id: "ffc52a8f-3d2a-4f3b-9c41-ee1cd9fcf290",
+    walletId: "",
     name: "Phish",
-    type: EntertainerType.Music,
-    description: "Phish will embark on a 23-date Summer Tour beginning July 11 and culminating with their traditional Labor Day Weekend run at Dick's Sporting Goods Park in Commerce City, CO, where they'll return for four nights. The tour includes a seven-night run at New York City's Madison Square Garden.",
-    currentIteration: "Summer Tour 2023",
+    type: EntertainerType.MUSIC,
+    description:
+      "Phish will embark on a 23-date Summer Tour beginning July 11 and culminating with their traditional Labor Day Weekend run at Dick's Sporting Goods Park in Commerce City, CO, where they'll return for four nights. The tour includes a seven-night run at New York City's Madison Square Garden.",
+    iteration: "Summer Tour 2023",
+    createdAt: "2024-01-25T15:18:33Z",
+    updatedAt: "2024-01-25T15:18:33Z",
   },
   "0.0.222": {
+    id: "cbd2987d-6202-4764-9b73-f5e1078c3c10",
+    walletId: "",
     name: "Kansas City Chiefs",
-    type: EntertainerType.Sports,
-    description: "Head Coach Andy Reid leads the returning Super Bowl champion Kansas City Chiefs into the 2023 season captained by the top quarterback in the NFL, Patrick Mahomes.",
-    currentIteration: "2023 NFL Season"
+    type: EntertainerType.SPORTS,
+    description:
+      "Head Coach Andy Reid leads the returning Super Bowl champion Kansas City Chiefs into the 2023 season captained by the top quarterback in the NFL, Patrick Mahomes.",
+    iteration: "2023 NFL Season",
+    createdAt: "2024-01-25T15:18:33Z",
+    updatedAt: "2024-01-25T15:18:33Z",
   },
   "0.0.333": {
+    id: "0df4aec3-dfa3-422e-8838-ef539d35ba53",
+    walletId: "",
     name: "Tim Heidecker",
-    type: EntertainerType.Comedy,
-    description: "Tim Heidecker is bringing his \"No More Bullshit\" stand-up character as well as The Very Good Band (with fan favorite Vic Berger) back to the USA for a brand new Summer Tour!",
-    currentIteration: "The Two Tims Summer Tour '23",
+    type: EntertainerType.COMEDY,
+    description:
+      'Tim Heidecker is bringing his "No More Bullshit" stand-up character as well as The Very Good Band (with fan favorite Vic Berger) back to the USA for a brand new Summer Tour!',
+    iteration: "The Two Tims Summer Tour '23",
+    createdAt: "2024-01-25T15:18:33Z",
+    updatedAt: "2024-01-25T15:18:33Z",
   },
 };
